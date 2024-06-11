d63e5f3ecd542864136008fd1c089ae3d5af775f
alexmingikim <alex.kim9905@gmail.com>
2024-06-12 10:33:59 +1200

    Specifying 5 states for execs script

--- generate_execs.py
+++ generate_execs.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Generate date range for each week of 2017 and 2019
 weeks = pd.date_range(start="2017-06-01", end="2019-06-01", freq="W")
-states = ["Georgia"]
+states = ["Georgia", "NewYork", "Oregon", "Texas", "Wyoming"]
 
 # Generate command strings for each date
 commands = []
