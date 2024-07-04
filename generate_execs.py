50f6034536c891b1c927e449cfbdb417136a4178
alexmingikim <alex.kim9905@gmail.com>
2024-07-04 19:03:57 +1200

    Debugging

--- generate_execs.py
+++ generate_execs.py
@@ -13,8 +13,8 @@ commands = []
 for state in states:
     for date in weeks:
         commands.append(
-            f"srun --unbuffered python timegpt_script.py {state} {date.strftime('%Y-%m-%d')}" #
-        )
+            f"srun --unbuffered python timegpt_script.py {state} {date.strftime('%Y-%m-%d')}"
+        ) #
 
 # Save command strings to a text file with an empty line at the end
 with open("execs/train_models", "w") as file:
