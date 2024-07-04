a6f0350910179ae4f7e36074c52ad5d4ee09c1c2
alexmingikim <alex.kim9905@gmail.com>
2024-07-04 19:42:00 +1200

    200 fine-tuning steps

--- generate_summary_evaluation.py
+++ generate_summary_evaluation.py
@@ -9,7 +9,7 @@ horizons = ['1week', '4week', '13week', '26week', '52week'] #
 # Loop over each forecast horizon
 for state in states: 
     for horizon in horizons:
-        root_dir = f'output/hyperparameter/150/{state}' #
+        root_dir = f'output/hyperparameter/200/{state}' #
         eval_dir = os.path.join(root_dir, horizon, 'evaluation')
         
         # Read all csv files in evaluation directory
