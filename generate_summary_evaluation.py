a24d161a5069a9236ddb43b58c2aedbd02ce92ab
alexmingikim <alex.kim9905@gmail.com>
2024-07-09 23:05:09 +1200

    TX - 100 finetune steps

--- generate_summary_evaluation.py
+++ generate_summary_evaluation.py
@@ -3,14 +3,13 @@ import pandas as pd
 
 # Define states and forecast horizons
 # TODO: specify states and horizons as environmental variables
-states = ["California"] #
-# states = ["Texas"] #
+states = ["Texas"] #
 horizons = ['1week', '4week', '13week', '26week', '52week'] #
 
 # Loop over each forecast horizon
 for state in states: 
     for horizon in horizons:
-        root_dir = f'output/%UNWEIGHTED ILI/hyperparameter_tuning/500/{state}' #
+        root_dir = f'output/%UNWEIGHTED ILI/hyperparameter_tuning/100/{state}' #
         eval_dir = os.path.join(root_dir, horizon, 'evaluation')
         
         # Read all csv files in evaluation directory
