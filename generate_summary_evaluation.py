72fa8b5a356cf585be382dad9c6772e5f3c6e7af
alexmingikim <alex.kim9905@gmail.com>
2024-07-10 00:55:52 +1200

    Adding IQR to evaluation script

--- generate_summary_evaluation.py
+++ generate_summary_evaluation.py
@@ -1,15 +1,30 @@
 import os
 import pandas as pd
+import numpy as np
+
+# Function to compute IQR
+def compute_iqr(column):
+    q3, q1 = np.percentile(column, [75 ,25])
+    return q3 - q1
+
+# Alternative function to compute IQR
+# source: https://stackoverflow.com/questions/51943661/is-scipy-stats-doing-wrong-calculation-for-iqr
+
+# def compute_iqr(m):
+#     m = np.array(m)
+#     n = m.size//2
+#     m_ = np.partition(m.ravel(), n + 1)
+#     return np.median(m_[n + m.size%2:]) - np.median(m_[:n])
 
 # Define states and forecast horizons
 # TODO: specify states and horizons as environmental variables
-states = ["Texas"] #
+states = ["California", "Texas"] #
 horizons = ['1week', '4week', '13week', '26week', '52week'] #
 
 # Loop over each forecast horizon
 for state in states: 
     for horizon in horizons:
-        root_dir = f'output/%UNWEIGHTED ILI/hyperparameter_tuning/500/{state}' #
+        root_dir = f'output/%UNWEIGHTED ILI/zeroshot/{state}' #
         eval_dir = os.path.join(root_dir, horizon, 'evaluation')
         
         # Read all csv files in evaluation directory
@@ -19,7 +34,7 @@ for state in states:
                 file_path = os.path.join(eval_dir, file)
                 df = pd.read_csv(file_path)
                 metrics.append(df[['MAE', 'MAPE', 'sMAPE', 'RMSE', 'MASE']]) #
-        
+
         # Concatenate all dataframes
         if metrics:
             all_metrics = pd.concat(metrics)
@@ -27,6 +42,13 @@ for state in states:
             # Compute average of the metrics
             avg_metrics = all_metrics.mean().to_frame().T
             
+            # Compute IQR of the metrics
+            iqr_metrics = all_metrics.apply(compute_iqr).to_frame().T
+            
+            # Combine average and IQR metrics
+            summary_metrics = pd.concat([avg_metrics, iqr_metrics])
+            summary_metrics.index = ['Average', 'IQR']
+            
             # Write summary to csv file
             summary_file_path = os.path.join(root_dir, horizon, 'summary_evaluation.csv')
-            avg_metrics.to_csv(summary_file_path, index=False)
+            summary_metrics.to_csv(summary_file_path)
