1d62e1f80cf2b4e6be7e7ce37de86c51ce336535
alexmingikim <alex.kim9905@gmail.com>
2024-07-10 11:03:27 +1200

    Debugging

--- plotting_aggregated_1week.py
+++ plotting_aggregated_1week.py
@@ -4,16 +4,20 @@ import pandas as pd
 states = ["California", "Texas"]
 
 for state in states:
-    root_dir = f'output/%UNWEIGHTED ILI/zeroshot/{state}/1week/forecasts' #
+    root_dir = f'output/%UNWEIGHTED ILI/zeroshot/{state}/1week/forecasts'
+    aggregated_df = pd.DataFrame()
 
     for filename in os.listdir(root_dir):
         if filename.endswith(".csv"):
-            aggregated_df = pd.DataFrame()
             file_path = os.path.join(root_dir, filename)
             df = pd.read_csv(file_path)
             aggregated_df = pd.concat([aggregated_df, df])
 
-    # Reset index after concatenation
+    # Convert 'Split_week' to datetime and sort by date
+    aggregated_df['Split_week'] = pd.to_datetime(aggregated_df['Split_week'])
+    aggregated_df = aggregated_df.sort_values(by='Split_week')
+
+    # Reset index after sorting
     aggregated_df.reset_index(drop=True, inplace=True)
 
     # Save aggregated forecasts as csv
