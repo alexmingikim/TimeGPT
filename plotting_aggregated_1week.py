a4c4af824f737664953cfa7ec76a16127825f703
alexmingikim <alex.kim9905@gmail.com>
2024-07-10 10:55:36 +1200

    Plotting script

---
+++ plotting_aggregated_1week.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+states = ["California", "Texas"]
+
+for state in states:
+    root_dir = f'output/%UNWEIGHTED ILI/zeroshot/{state}/1week/forecasts' #
+
+    for filename in os.listdir(root_dir):
+        if filename.endswith(".csv"):
+            aggregated_df = pd.DataFrame()
+            file_path = os.path.join(root_dir, filename)
+            df = pd.read_csv(file_path)
+            aggregated_df = pd.concat([aggregated_df, df])
+
+    # Reset index after concatenation
+    aggregated_df.reset_index(drop=True, inplace=True)
+
+    # Save aggregated forecasts as csv
+    aggregated_df.to_csv('aggregated_forecasts.csv', index=False)
