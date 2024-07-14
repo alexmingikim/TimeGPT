72baff2eea7507bdc1a548f1f32c9ab71455956e
alexmingikim <alex.kim9905@gmail.com>
2024-07-14 17:10:11 +1200

    Modifying summary evaluation script for winter analysis

--- generate_summary_evaluation.py
+++ generate_summary_evaluation.py
@@ -19,13 +19,13 @@ def compute_iqr(column):
 # Define states and forecast horizons
 # TODO: specify states and horizons as environmental variables
 states = ["California", "Texas"] #
-horizons = ['1week', '4week', '13week', '26week', '52week'] #
+horizons = ['1', '4', '13', '26', '52'] #
 
 # Loop over each forecast horizon
 for state in states: 
     for horizon in horizons:
-        root_dir = f'output/%UNWEIGHTED ILI/zeroshot_covariates/{state}' #
-        eval_dir = os.path.join(root_dir, horizon, 'evaluation')
+        root_dir = f'output/%UNWEIGHTED ILI/zeroshot/all/{state}' #
+        eval_dir = os.path.join(root_dir, horizon+'week', 'evaluation')
         
         # Read all csv files in evaluation directory
         metrics = []
@@ -50,5 +50,36 @@ for state in states:
             summary_metrics.index = ['Average', 'IQR']
             
             # Write summary to csv file
-            summary_file_path = os.path.join(root_dir, horizon, 'summary_evaluation.csv')
+            summary_file_path = os.path.join(root_dir, horizon+'week', 'summary_evaluation.csv')
+            summary_metrics.to_csv(summary_file_path)
+    
+    ### Winter analysis
+    for horizon in [1,4]:
+        root_dir = f'output/%UNWEIGHTED ILI/zeroshot/winter/{state}' #
+        eval_dir = os.path.join(root_dir, horizon+'week', 'evaluation')
+        
+        # Read all csv files in evaluation directory
+        metrics = []
+        for file in os.listdir(eval_dir):
+            if file.endswith('.csv'):
+                file_path = os.path.join(eval_dir, file)
+                df = pd.read_csv(file_path)
+                metrics.append(df[['MAE', 'MAPE', 'sMAPE', 'RMSE', 'MASE']]) #
+
+        # Concatenate all dataframes
+        if metrics:
+            all_metrics = pd.concat(metrics)
+            
+            # Compute average of the metrics
+            avg_metrics = all_metrics.mean().to_frame().T
+            
+            # Compute IQR of the metrics
+            iqr_metrics = all_metrics.apply(compute_iqr).to_frame().T
+            
+            # Combine average and IQR metrics
+            summary_metrics = pd.concat([avg_metrics, iqr_metrics])
+            summary_metrics.index = ['Average', 'IQR']
+            
+            # Write summary to csv file
+            summary_file_path = os.path.join(root_dir, horizon+'week', 'summary_evaluation.csv')
             summary_metrics.to_csv(summary_file_path)
