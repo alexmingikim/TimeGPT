3632e68480bf313fe1ed1911a40fec18dd596899
alexmingikim <alex.kim9905@gmail.com>
2024-07-14 19:09:13 +1200

    Aggregate script

---
+++ aggregate_script.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+
+# TODO: include real data
+
+states = ["California", "Texas"]
+horizons = ["1week", "4week"]
+winter_months = [11, 12, 1, 2, 3, 4] #
+
+for state in states:
+    for horizon in horizons: 
+        base_dir = f'output/%UNWEIGHTED ILI/zeroshot/all/{state}/{horizon}'
+        root_dir = f'output/%UNWEIGHTED ILI/zeroshot/all/{state}/{horizon}/forecasts'
+        aggregated_df = pd.DataFrame()
+
+        for filename in os.listdir(root_dir):
+            if filename.endswith(".csv"):
+                file_path = os.path.join(root_dir, filename)
+                df = pd.read_csv(file_path)
+                aggregated_df = pd.concat([aggregated_df, df])
+
+        # Convert 'Split_week' to datetime and sort by date
+        aggregated_df['Split_week'] = pd.to_datetime(aggregated_df['Split_week'])
+        aggregated_df = aggregated_df.sort_values(by='Split_week')
+
+        # Extract forecasts for winter months
+        aggregated_df['Month'] = aggregated_df['Split_week'].dt.month
+        filtered_forecasts = aggregated_df[aggregated_df['Month'].isin(winter_months)]
+        filtered_forecasts = filtered_forecasts.drop(columns=['Month'])
+
+        # Reset index after sorting
+        aggregated_df.reset_index(drop=True, inplace=True)
+
+        # Save aggregated forecasts as csv
+        output_file_path = os.path.join(base_dir, 'aggregated_forecasts_winter.csv')
+        aggregated_df.to_csv(output_file_path, index=False)
