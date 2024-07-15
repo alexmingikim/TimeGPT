fdcea4535bfc7232e2733d17fc6bbcd41c76c517
alexmingikim <alex.kim9905@gmail.com>
2024-07-15 18:00:14 +1200

    1

--- aggregate_script.py
+++ aggregate_script.py
@@ -4,13 +4,13 @@ import pandas as pd
 # TODO: include real data
 
 states = ["California", "Texas"]
-horizons = ["4week"]
-winter_months = [11, 12, 1, 2, 3, 4]
+horizons = ["1week"]
+# winter_months = [11, 12, 1, 2, 3, 4]
 
 for state in states:
     for horizon in horizons:
-        base_dir = f'output/%UNWEIGHTED ILI/zeroshot_covariates/{state}/{horizon}'
-        root_dir = f'output/%UNWEIGHTED ILI/zeroshot_covariates/{state}/{horizon}/forecasts'
+        base_dir = f'output/%UNWEIGHTED ILI/zeroshot/{state}/{horizon}'
+        root_dir = f'output/%UNWEIGHTED ILI/zeroshot/{state}/{horizon}/forecasts'
         aggregated_df = pd.DataFrame()
 
         for filename in os.listdir(root_dir):
@@ -24,13 +24,13 @@ for state in states:
         aggregated_df = aggregated_df.sort_values(by='Split_week')
 
         # Extract forecasts for winter months
-        aggregated_df['Month'] = aggregated_df['Split_week'].dt.month
-        filtered_forecasts = aggregated_df[aggregated_df['Month'].isin(winter_months)]
-        filtered_forecasts = filtered_forecasts.drop(columns=['Month'])
+        # aggregated_df['Month'] = aggregated_df['Split_week'].dt.month
+        # filtered_forecasts = aggregated_df[aggregated_df['Month'].isin(winter_months)]
+        # filtered_forecasts = filtered_forecasts.drop(columns=['Month'])
 
         # Reset index after filtering
-        filtered_forecasts.reset_index(drop=True, inplace=True)
+        aggregated_df.reset_index(drop=True, inplace=True)
 
         # Save filtered forecasts as csv
-        output_file_path = os.path.join(base_dir, '4wk_covariates.csv')
-        filtered_forecasts.to_csv(output_file_path, index=False)
+        output_file_path = os.path.join(base_dir, '1wk_aggregated.csv')
+        aggregated_df.to_csv(output_file_path, index=False)
