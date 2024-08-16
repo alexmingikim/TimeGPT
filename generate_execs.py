abe670cbd5e713ec02cc82b1dad2de50c1effb8d
alexmingikim <alex.kim9905@gmail.com>
2024-08-16 12:24:48 +1200

    updating generate_execs

--- generate_execs.py
+++ generate_execs.py
@@ -6,7 +6,7 @@ end_date = "2019-06-01"
 
 # Generate date range for each week of 2017 and 2019
 weeks = pd.date_range(start=start_date, end=end_date, freq="W-MON")
-states = ["California", "Texas"] #
+states = ["Texas"] #
 
 # Generate command strings for each date
 commands = []
